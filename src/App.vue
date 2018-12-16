c5f66064e803a2cccda8bf5de1bc850805547a38
Meredith Fontana <meredith@Merediths-MacBook-Pro.local>
2018-12-15 19:17:43 -0700

    added css to style skills.vue component and main app

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,18 @@ export default {
 
 <style scoped>
 body {
-  background-color: #eeeeee
+  background-color: #EEEEEE;
+  font-family: 'Montserrat', sans-serif;
+  display: grid;
+  grid-template-rows: auto;
+  justify-items: center;
+  padding-top: 50px;
+}
+body, html {
+  margin: 0;
+  height: 100%;
+}
+#app {
+    width: 50%;
 }
 </style>
